de15dd6f6d8d04edd30bcc39a0a498b7b3a782bf
futureunicorne <elhadi-hamza@hotmail.fr>
2016-10-28 02:50:40 +0200

    ex05d05x2

--- day05/ex05/ft_strstr.c
+++ day05/ex05/ft_strstr.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+ #include <stdio.h>
+#include <string.h>
 
 char	*ft_strstr(char *str, char *to_find)
 {
@@ -6,21 +7,16 @@ char	*ft_strstr(char *str, char *to_find)
 	int	j;
 
 	i = 0;
-	j = 0;
 	while (str[i] != '\0')
 	{
-		while (str[i] == to_find[j])
+		j = 0;
+		while (str[i + j] == to_find[j])
 		{
-			i++;
 			j++;
 			if (to_find[j] == '\0')
-				return (to_find);
+				return (str + i);
 		}
 		i++;
-	}
-	if(str[i] == '\0')
-	{
-		return(NULL);
 	}
 	return (0);
 }
@@ -30,8 +26,9 @@ int main(void)
   char *meule;
 
   str = "lenovastream";
-  meule = "stream";
+  meule = "astream";
 	ft_strstr(str, meule);
-  printf("%s\n ", ft_strstr(str, meule));
+  printf("%s\n", ft_strstr(str, meule));
+	printf("%s\n", strstr(str, meule));
   return(0);
 }
