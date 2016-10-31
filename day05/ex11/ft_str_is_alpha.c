edfad6cc001bb906ac462bdd1eff15f784d7e71e
futureunicorne <elhadi-hamza@hotmail.fr>
2016-11-01 00:35:38 +0100

    ex11d05

---
+++ day05/ex11/ft_str_is_alpha.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int ft_str_is_alpha(char *str)
+{
+  int i;
+
+  i = 0;
+  if(!str)
+  {
+    return(1);
+  }
+  while(str[i] != '\0')
+  {
+    if(!(str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+    {
+      return(0);
+    }
+    i++;
+  }
+  return(1);
+}
+
+int main(void)
+{
+  char *str;
+
+  str = "fjllew";
+  ft_str_is_alpha(str);
+  printf("%d\n",ft_str_is_alpha(str));
+  return(0);
+}
