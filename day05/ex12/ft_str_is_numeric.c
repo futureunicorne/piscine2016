0d6946e46b90aaabbc63ca24738390711b55ccf6
futureunicorne <elhadi-hamza@hotmail.fr>
2016-11-01 00:40:06 +0100

    ex12d05

---
+++ day05/ex12/ft_str_is_numeric.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int ft_str_is_numeric(char *str)
+{
+  int i;
+
+  i = 0;
+  if(!str)
+  {
+    return(1);
+  }
+  while(str[i] != '\0')
+  {
+    if(!(str[i] >= '0' && str[i] <= '9'))
+    {
+      return(0);
+    }
+    i++;
+  }
+  return(1);
+}
+
+int main(void)
+{
+  char *str;
+
+  str = "";
+  ft_str_is_numeric(str);
+  printf("%d\n",ft_str_is_numeric(str));
+  return(0);
+}
